5833e652bad7ae9d0871f7843087068e666a94b3
ucitelskyPocitacVP3 <student@158.197.35.78>
2016-12-20 08:45:51 +0100

    Cvicenie 20. 12. 2016
    Pridana databaza

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,16 +1,78 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: debilnicek
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
 
-CREATE TABLE kategoria (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-    nazov VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id) 
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
-ALTER TABLE uloha ADD COLUMN kategoria_id INTEGER NOT NULL DEFAULT 0;
+-- Table structure for table `kategoria`
 --
-INSERT INTO kategoria VALUES
-(0, 'Nezaradené');
+
+DROP TABLE IF EXISTS `kategoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kategoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nazov` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
-UPDATE kategoria SET id = 0 WHERE id = 1;
+-- Dumping data for table `kategoria`
+--
 
+LOCK TABLES `kategoria` WRITE;
+/*!40000 ALTER TABLE `kategoria` DISABLE KEYS */;
+INSERT INTO `kategoria` VALUES (0,'Nezaradené'),(13,'Veci'),(14,'Zabava'),(15,'Java'),(16,'Jedlo'),(17,'Java 9');
+/*!40000 ALTER TABLE `kategoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uloha`
+--
+
+DROP TABLE IF EXISTS `uloha`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uloha` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `popis` varchar(500) NOT NULL,
+  `termin` datetime DEFAULT NULL,
+  `stav` tinyint(1) NOT NULL,
+  `kategoria_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uloha`
+--
+
+LOCK TABLES `uloha` WRITE;
+/*!40000 ALTER TABLE `uloha` DISABLE KEYS */;
+INSERT INTO `uloha` VALUES (1,'ísť domov',NULL,0,0),(2,'informatikovica','2016-11-09 17:00:00',1,0),(3,'oslavovať MysqlDao',NULL,1,0),(4,'',NULL,0,0),(5,'Programovať debilníček',NULL,0,14),(6,'Have fun',NULL,0,13),(7,'Mikuláš',NULL,0,14),(8,'Mikuláš',NULL,0,0),(9,'Kapustnica',NULL,0,0),(10,'Kapustnica',NULL,0,0);
+/*!40000 ALTER TABLE `uloha` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-20  8:44:48
